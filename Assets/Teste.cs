ce0d4f33a2a2805d37219bbd9f2a83eb5c307fd3
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2021-03-15 11:03:22 -0300

    15-03-21

--- Assets/Teste.cs
+++ Assets/Teste.cs
@@ -2,14 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct FichaDePersonagem{
+    public string nome;
+    public int hp;
+    Teste.Naipes np;
+    public void Mostrar(){
+        Debug.Log("Meu nome é: "+nome+ "  e meu HP é: "+ hp);
+    }
+}
+
 public class Teste : MonoBehaviour
 {
+    public float timer = 0;
+    public float timeToReload = 1;
+    public int q;
     //public string[]nomes = new string[10];
     public string[] nomes;
     //LISTAS,PILHAS E FILAS
     public Stack<int> deck = new Stack<int>();
     public Queue<Color> bolhas = new Queue<Color>();
-
+    public List<string> guns = new List<string>();
+    public Dictionary<string,int> enemys = new Dictionary<string, int>();
+    public enum Naipes{OUROS=10,COPAS=20,ESPADAS=30,PAUS=40}
+    FichaDePersonagem f;
     // Start is called before the first frame update
     void Start()
     {
@@ -47,7 +62,7 @@ public class Teste : MonoBehaviour
         }
         */
         /*
-        while(n>=0){
+        while(true){
             Debug.Log(n);
             //n = n+2;
             //n+=2;
@@ -55,7 +70,16 @@ public class Teste : MonoBehaviour
             //n=n-1;
             //n-=1;
             n--;
+
+            if(n==null){
+                break;
+            }
         }
+        */
+        /*
+        do{
+            //AÇAO
+        }while(n>=0);
         */
         /*
         for(int i=50; i>=0; i-=2){
@@ -82,7 +106,7 @@ public class Teste : MonoBehaviour
         deck.Pop();
         Debug.Log(deck.Peek());
         */
-
+        /*
         bolhas.Enqueue(Color.red);
         bolhas.Enqueue(Color.green);
         bolhas.Enqueue(Color.blue);
@@ -92,12 +116,52 @@ public class Teste : MonoBehaviour
         //Debug.Log(bolhas.Peek());
         Debug.Log(bolhas.Dequeue());
         //Debug.Log(bolhas.Peek());
-         Debug.Log(bolhas.Dequeue());
+        Debug.Log(bolhas.Dequeue());
+        */
+        /*
+        guns.Add("UMP45");
+        guns.Add("GLOCK");
+        guns.Add("AK47");
+        guns.Add("AWP");
+
+        guns.Sort();
+        guns.Insert(1,"DESERTE");
+
+        //guns.Reverse();
+
+        //guns.Remove("AWP");
+        //guns.RemoveAt(2);
+        Debug.Log(guns.Contains("AWP"));
+
+        int c = guns.Count;
+
+        for(int i = 0; i < c;i++){
+            Debug.Log(guns[i]);
+        }
+        guns.Clear();
+        */
+        /*
+        enemys.Add("Orc", 100);
+        enemys.Add("Troll", 200);
+        enemys.Add("Beholder", 500);
+
+        Debug.Log(enemys.Count);
+        Debug.Log(enemys.ContainsKey("Beholder"));
+        */
+        f.nome = "Kleber";
+        f.hp = 100;
+        f.Mostrar();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        //Debug.Log(timer);
+        if(timer >= timeToReload){
+           //Debug.Log(q);
+           q++;
+           timer = 0;
+        }
     }
 }
