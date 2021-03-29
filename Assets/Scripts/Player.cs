c28a828c78433432e48fb3e0ba3ddcc6b7cd8396
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2021-03-29 11:00:18 -0300

    29-03-21

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    public float moveSpeed = 2;
+
+    public float distance = 2;
+    public float distanceNear = 0.5f;
+    
+    public LayerMask enemyLayer = 3;
+
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //playerLayer = ~playerLayer;
+        //Collider bala = Instantiate(prefab);
+        //Physics.IgnoreCollision(transform.GetComponent<Collider>(), bala,true);
+        //gameObject.tag = "Enemy";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector3(rb.velocity.x,rb.velocity.y, h*moveSpeed);
+
+        RaycastHit hit;
+        if(Input.GetButtonDown("Fire1")){
+            if(Physics.Raycast(transform.position, Vector3.forward, out hit, distance,enemyLayer)){
+                Debug.DrawRay(transform.position,Vector3.forward*distance,Color.red);
+                //if(hit.transform.tag == "Enemy")
+                    //Debug.Log("Acertou Miseravi");
+            }else{
+                Debug.DrawRay(transform.position,Vector3.forward*distance,Color.yellow);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        IDamageable obj = other.GetComponent<IDamageable>();
+
+        if(obj != null){
+            obj.TakeDamage();
+            score+=10;
+        }
+    }
+}
